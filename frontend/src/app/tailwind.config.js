cf9b8d6624f0c321782b989b2f409da0f061f68b
DanielAlbq <175652@upf.br>
2025-04-13 21:39:38 -0300

    Criação de pastas e configuração do tailwind

---
+++ frontend/src/app/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}", //inclusao pasta app
+        "./components/**/*.{js,ts,jsx,tsx}", //inclusao pasta components
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
